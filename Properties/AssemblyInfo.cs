4aa47326fa2f2ba28871740f4bd110b59c538e7b
campuslogic <github@campuslogic.com>
2022-11-08 18:25:20 +0000

    CLConnect 3.13.7.8224

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CampusLogicEvents.Web")]
-[assembly: AssemblyDescription("Build: 3.13.7.6648 - Commit: 33168")]
+[assembly: AssemblyDescription("Build: 3.13.7.8224 - Commit: 33279")]
 [assembly: AssemblyConfiguration("release")]
 [assembly: AssemblyCompany("CampusLogic")]
 [assembly: AssemblyProduct("CL Connect")]
